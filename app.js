c3e3fb74e7a733cb9f9d46046af9be82664706c2
Syed-Kumail-Naqvi <syedkumailnaqvi82@gmail.com>
2024-09-25 13:08:41 +0500

    Table Printing

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var userValue = prompt("Enter A Number To Generate It's Table:");
+var userValue = +prompt("Enter A Number To Generate It's Table:");
 
-for (var i = userValue; i >= 1; i--) {
-    for (var j = 1 ; j <= 10 ; j++){
-        console.log(i + " x " + j + " = " + i * j);
+for (var i = 1; i<= userValue; userValue--) {
+    for (var j = 1 ; j <= 10; j++){
+        document.write("<br>", userValue + " x " + j + " = " + userValue * j);   
     }
-    // document.getElementById("table").innerHTML = [userValue.value + " x " + i + " = " + (i*userValue).value];
-};
+        document.write("<br>")
+}
